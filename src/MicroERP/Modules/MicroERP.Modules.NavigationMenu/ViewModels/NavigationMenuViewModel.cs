516e1b2837786307e8197cd547ce917c34bcbb02
Anatoly Severin <severin@pointcad.ru>
2020-08-18 00:34:11 +0300

    triger sonar cloud

--- src/MicroERP/Modules/MicroERP.Modules.NavigationMenu/ViewModels/NavigationMenuViewModel.cs
+++ src/MicroERP/Modules/MicroERP.Modules.NavigationMenu/ViewModels/NavigationMenuViewModel.cs
@@ -5,15 +5,16 @@ namespace MicroERP.Modules.NavigationMenu.ViewModels
     public class NavigationMenuViewModel : BindableBase
     {
         private string _message;
-        public string Message
-        {
-            get { return _message; }
-            set { SetProperty(ref _message, value); }
-        }
 
         public NavigationMenuViewModel()
         {
             Message = "View A from your Prism Module";
+        }
+
+        public string Message
+        {
+            get => _message;
+            set => SetProperty(ref _message, value);
         }
     }
 }
